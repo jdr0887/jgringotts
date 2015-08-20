04427fc8cba372d337191eb549c245405af631de
Jason Reilly <jdr0887@gmail.com>
2015-08-20 15:26:58 -0400

    refactor to have editable listview

--- jgringotts-jfx/src/main/java/com/kiluet/jgringotts/MainController.java
+++ jgringotts-jfx/src/main/java/com/kiluet/jgringotts/MainController.java
@@ -1,12 +1,54 @@
 package com.kiluet.jgringotts;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.controlsfx.dialog.Dialogs;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
-public class MainController {
+import com.kiluet.jgringotts.dao.ItemDAO;
+import com.kiluet.jgringotts.dao.JGringottsDAOException;
+import com.kiluet.jgringotts.dao.model.Item;
+
+public class MainController implements Initializable {
+
+    private final KeyCombination saveKeyCombination = new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN);
+
+    @FXML
+    private ListView<String> itemListView;
+
+    @FXML
+    private TextArea itemContentTextArea;
+
+    private final ContextMenu contextMenu = new ContextMenu();
+
+    private ObservableList<String> observableList = FXCollections.observableArrayList();
 
     private JGringotts app;
 
@@ -14,37 +56,223 @@ public class MainController {
         super();
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        MenuItem addMenuItem = new MenuItem("Add");
+        addMenuItem.setOnAction(addListItemHandler);
+        contextMenu.getItems().add(addMenuItem);
+
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        deleteMenuItem.setOnAction(deleteListItemHandler);
+        contextMenu.getItems().add(deleteMenuItem);
+
+        itemListView.setCellFactory(TextFieldListCell.forListView());
+        itemListView.setItems(observableList);
+        itemListView.addEventHandler(MouseEvent.MOUSE_CLICKED, showContextMenuHandler);
+
+        observableList.addListener(itemListViewChangeHandler);
+
+    }
+
     @FXML
     private void showAbout(final ActionEvent event) {
-        app.showAbout();
+        Dialogs.create().masthead("JGringotts").message(String.format("Version: %s", app.getVersion()))
+                .showInformation();
     }
 
     @FXML
-    private void doNew(final ActionEvent event) {
-        app.doNew();
+    private void onEditCommit(ListView.EditEvent<String> event) {
+
+        try {
+            ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+            Item item = itemDAO.findByName(itemListView.getSelectionModel().getSelectedItem());
+            String newValue = event.getNewValue();
+            item.setName(newValue);
+            itemDAO.save(item);
+            itemListView.getItems().set(event.getIndex(), event.getNewValue());
+            FXCollections.sort(observableList, (String a, String b) -> a.compareTo(b));
+            itemContentTextArea.setText(item.getDescription());
+        } catch (JGringottsDAOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @FXML
-    private void doExport(final ActionEvent event) {
-        app.doExport();
+    public void doNew(final ActionEvent event) {
+
+        try {
+            ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+            Item item = new Item("New");
+            item.setId(itemDAO.save(item));
+            observableList.add(item.getName());
+            FXCollections.sort(observableList, (String a, String b) -> a.compareTo(b));
+            itemContentTextArea.setText("");
+        } catch (JGringottsDAOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @FXML
-    private void doImport(final ActionEvent event) {
-        app.doImport();
+    public void doExport(final ActionEvent event) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        chooser.setTitle("Export Items");
+        File directory = chooser.showDialog(app.getStage());
+        ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+        try {
+            List<Item> itemList = itemDAO.findAll();
+            for (Item item : itemList) {
+                File f = new File(directory, item.getName());
+                FileUtils.writeStringToFile(f, item.getDescription());
+            }
+        } catch (JGringottsDAOException | IOException e1) {
+            e1.printStackTrace();
+        }
     }
 
     @FXML
-    private void doExit(final ActionEvent event) {
+    public void doImport(final ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        chooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        chooser.setTitle("Import Items");
+        List<File> fileList = chooser.showOpenMultipleDialog(app.getStage());
+        ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+        if (fileList != null && !fileList.isEmpty()) {
+            for (File file : fileList) {
+                try {
+                    Item item = new Item();
+                    item.setName(file.getName());
+                    item.setDescription(FileUtils.readFileToString(file));
+                    itemDAO.save(item);
+                    observableList.add(item.getName());
+                } catch (IOException | JGringottsDAOException e) {
+                    e.printStackTrace();
+                }
+            }
+            FXCollections.sort(observableList, (String a, String b) -> a.compareTo(b));
+            itemListView.setItems(observableList);
+        }
+    }
+
+    @FXML
+    public void doExit(final ActionEvent event) {
         System.exit(0);
     }
+
+    @FXML
+    public void itemContentTextAreaKeyHandler(KeyEvent event) {
+
+        if (event.isControlDown()) {
+            boolean matched = saveKeyCombination.match(event);
+            if (matched) {
+                ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+                try {
+                    Item item = itemDAO.findByName(itemListView.getSelectionModel().getSelectedItem());
+                    item.setDescription(itemContentTextArea.getText());
+                    itemDAO.save(item);
+                } catch (JGringottsDAOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private EventHandler<ActionEvent> deleteListItemHandler = new EventHandler<ActionEvent>() {
+
+        @Override
+        public void handle(ActionEvent event) {
+            ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+            try {
+                String name = itemListView.getSelectionModel().getSelectedItem();
+                if (StringUtils.isNotEmpty(name)) {
+                    Item item = itemDAO.findByName(name);
+                    itemDAO.delete(item);
+                    observableList.remove(name);
+                    FXCollections.sort(observableList, (String a, String b) -> a.compareTo(b));
+                    if (!itemListView.getItems().isEmpty()) {
+                        name = itemListView.getSelectionModel().getSelectedItem();
+                        item = itemDAO.findByName(name);
+                        if (item != null && StringUtils.isNotEmpty(item.getDescription())) {
+                            itemContentTextArea.setText(item.getDescription());
+                        }
+                    }
+                }
+            } catch (JGringottsDAOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    };
+
+    private EventHandler<ActionEvent> addListItemHandler = new EventHandler<ActionEvent>() {
+
+        @Override
+        public void handle(ActionEvent event) {
+            contextMenu.hide();
+            doNew(event);
+        }
+
+    };
+
+    private EventHandler<MouseEvent> showContextMenuHandler = new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent event) {
+            if (event.getButton() == MouseButton.SECONDARY) {
+                contextMenu.show(itemListView, event.getScreenX(), event.getScreenY());
+            } else {
+                contextMenu.hide();
+            }
+        }
+
+    };
+
+    @FXML
+    private void itemListViewSelectionHandler(MouseEvent event) {
+        ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+        try {
+            String name = itemListView.getSelectionModel().getSelectedItem();
+            if (StringUtils.isNotEmpty(name)) {
+                Item item = itemDAO.findByName(name);
+                if (item != null && StringUtils.isNotEmpty(item.getDescription())) {
+                    itemContentTextArea.setText(item.getDescription());
+                } else {
+                    itemContentTextArea.setText("");
+                }
+            }
+        } catch (JGringottsDAOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ListChangeListener<String> itemListViewChangeHandler = new ListChangeListener<String>() {
+
+        @Override
+        public void onChanged(Change<? extends String> event) {
+            ItemDAO itemDAO = app.getDaoMgr().getDaoBean().getItemDAO();
+            try {
+                String name = itemListView.getSelectionModel().getSelectedItem();
+                if (StringUtils.isNotEmpty(name) && !observableList.isEmpty()) {
+                    Item item = itemDAO.findByName(name);
+                    if (item != null) {
+                        itemContentTextArea.setText(item.getDescription());
+                    }
+                }
+            } catch (JGringottsDAOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    };
 
     @FXML
     private void handleKeyInput(final InputEvent event) {
         if (event instanceof KeyEvent) {
             final KeyEvent keyEvent = (KeyEvent) event;
             if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.A) {
-                app.showAbout();
+                showAbout(null);
             }
         }
     }
@@ -55,6 +283,34 @@ public class MainController {
 
     public void setApp(JGringotts app) {
         this.app = app;
+    }
+
+    public ListView<String> getItemListView() {
+        return itemListView;
+    }
+
+    public void setItemListView(ListView<String> itemListView) {
+        this.itemListView = itemListView;
+    }
+
+    public TextArea getItemContentTextArea() {
+        return itemContentTextArea;
+    }
+
+    public void setItemContentTextArea(TextArea itemContentTextArea) {
+        this.itemContentTextArea = itemContentTextArea;
+    }
+
+    public ObservableList<String> getObservableList() {
+        return observableList;
+    }
+
+    public void setObservableList(ObservableList<String> observableList) {
+        this.observableList = observableList;
+    }
+
+    public ContextMenu getContextMenu() {
+        return contextMenu;
     }
 
 }
